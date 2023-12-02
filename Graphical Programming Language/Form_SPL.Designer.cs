af6b97c01bb20d30f9627977685a4171b5acd98e
Prashant Bajracharya <bprashant21@tbc.edu.np>
2023-12-02 14:37:06 +0545

    Created UI and renamed all the components.

---
+++ Graphical Programming Language/Form_SPL.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Graphical_Programming_Language
+{
+    partial class Form_SPL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_MultiCmd = new System.Windows.Forms.TextBox();
+            this.pnl_Paint = new System.Windows.Forms.Panel();
+            this.textBox_SingleCmd = new System.Windows.Forms.TextBox();
+            this.btn_Run = new System.Windows.Forms.Button();
+            this.btn_Syntax = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_MultiCmd
+            // 
+            this.textBox_MultiCmd.Location = new System.Drawing.Point(27, 30);
+            this.textBox_MultiCmd.Multiline = true;
+            this.textBox_MultiCmd.Name = "textBox_MultiCmd";
+            this.textBox_MultiCmd.Size = new System.Drawing.Size(451, 356);
+            this.textBox_MultiCmd.TabIndex = 0;
+            // 
+            // pnl_Paint
+            // 
+            this.pnl_Paint.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnl_Paint.Location = new System.Drawing.Point(503, 30);
+            this.pnl_Paint.Name = "pnl_Paint";
+            this.pnl_Paint.Size = new System.Drawing.Size(451, 356);
+            this.pnl_Paint.TabIndex = 1;
+            // 
+            // textBox_SingleCmd
+            // 
+            this.textBox_SingleCmd.Location = new System.Drawing.Point(27, 407);
+            this.textBox_SingleCmd.Name = "textBox_SingleCmd";
+            this.textBox_SingleCmd.Size = new System.Drawing.Size(451, 20);
+            this.textBox_SingleCmd.TabIndex = 2;
+            // 
+            // btn_Run
+            // 
+            this.btn_Run.Location = new System.Drawing.Point(27, 433);
+            this.btn_Run.Name = "btn_Run";
+            this.btn_Run.Size = new System.Drawing.Size(61, 23);
+            this.btn_Run.TabIndex = 3;
+            this.btn_Run.Text = "Run";
+            this.btn_Run.UseVisualStyleBackColor = true;
+            // 
+            // btn_Syntax
+            // 
+            this.btn_Syntax.Location = new System.Drawing.Point(104, 433);
+            this.btn_Syntax.Name = "btn_Syntax";
+            this.btn_Syntax.Size = new System.Drawing.Size(75, 23);
+            this.btn_Syntax.TabIndex = 4;
+            this.btn_Syntax.Text = "Syntax";
+            this.btn_Syntax.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(966, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // Form_SPL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 468);
+            this.Controls.Add(this.btn_Syntax);
+            this.Controls.Add(this.btn_Run);
+            this.Controls.Add(this.textBox_SingleCmd);
+            this.Controls.Add(this.pnl_Paint);
+            this.Controls.Add(this.textBox_MultiCmd);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form_SPL";
+            this.Text = "Simple Programming Language";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_MultiCmd;
+        private System.Windows.Forms.Panel pnl_Paint;
+        private System.Windows.Forms.TextBox textBox_SingleCmd;
+        private System.Windows.Forms.Button btn_Run;
+        private System.Windows.Forms.Button btn_Syntax;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+    }
+}
+
---
+++ Graphical Programming Language/Form_SPL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphical_Programming_Language
+{
+    public partial class Form_SPL : Form
+    {
+        public Form_SPL()
+        {
+            InitializeComponent();
+        }
+    }
+}
