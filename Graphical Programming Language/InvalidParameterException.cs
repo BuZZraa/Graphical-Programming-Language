4723af8d27f3a15e1a200e19b940802c77fda22f
Prashant Bajracharya <royaleplay645@gmail.com>
2023-12-15 11:47:45 +0545

    Added custom exception classes to handle error messages.

---
+++ Graphical Programming Language/InvalidParameterException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphical_Programming_Language
+{
+    /// <summary>
+    /// Invalid Parameter Exception class to catch invalid parameter being provided to the command.
+    /// </summary>
+    public class InvalidParameterException : Exception
+    {
+        /// <summary>
+        /// Intializes an instance of Invalid Parameter Exception class and sets the error message to the base class.
+        /// </summary>
+        /// <param name="message">Sets the error message.</param>
+        public InvalidParameterException(string message) : base(message) { }
+    }
+}
---
+++ Graphical Programming Language/MultipleParametersException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphical_Programming_Language
+{
+    /// <summary>
+    /// Multiple Parameters Exception class to catch the exception of not enough parameter being provided to the command.
+    /// </summary>
+    public class MultipleParametersException : Exception
+    {
+        /// <summary>
+        /// Intializes an instance of Multiple Parameters Exception class and sets the error message to the base class.
+        /// </summary>
+        /// <param name="message">Sets the error message.</param>
+        public MultipleParametersException(string message) : base(message) { }       
+    }
+}
